1f01270aa62366a17be2d87376c464fb43ee3430
sobaih12 <153304568+sobaih12@users.noreply.github.com>
2025-03-24 14:31:49 +0200

    Initial Commit

---
+++ Crete Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crete Task
+//
+//  Created by Mostfa Sobaih on 24/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crete Task/Crete_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crete_TaskApp.swift
+//  Crete Task
+//
+//  Created by Mostfa Sobaih on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Crete_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
